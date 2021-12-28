cc7357a16b7047509eab09da0d7ec026ad23a4db
tolluset <dlqud19@gmail.com>
2021-12-29 01:54:03 +0900

    chore: remove comment

--- stack-queue/bridge-truck/index.js
+++ stack-queue/bridge-truck/index.js
@@ -4,9 +4,6 @@ export default function solution(br, w, tw) {
   let cnt = 0;
 
   while (true) {
-    console.log("start", tw, b, cnt, b.length);
-
-    // 추가
     if (b.length === br) b.shift();
 
     if (tw.length !== 0) {
@@ -14,26 +11,17 @@ export default function solution(br, w, tw) {
       if (sum <= w) {
         const t = tw.shift();
         b.push(t);
-        console.log("add");
       } else {
-        // 전진
         b.push(null);
-        console.log("move forward");
       }
     } else {
-      // 탈출
       b.push(null);
-      console.log("escape end");
     }
 
     cnt++;
 
     if (tw.length === 0 && b.every((_) => _ === null)) break;
-
-    console.log("end", tw, b, cnt, b.length);
   }
 
-  answer = cnt;
-
-  return answer;
+  return cnt;
 }
