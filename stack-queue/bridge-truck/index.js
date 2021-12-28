713f24cc1dc580ccd00cf170569139302b0887d1
tolluset <dlqud19@gmail.com>
2021-12-29 01:52:10 +0900

    refactor: use shift first

--- stack-queue/bridge-truck/index.js
+++ stack-queue/bridge-truck/index.js
@@ -1,11 +1,3 @@
-/*
-
-1. 트럭이 다리에 오름 +1
-2. 트럭이 한칸 전진 만약 이
-
-- 트럭 전진
-*/
-
 export default function solution(br, w, tw) {
   let answer = 0;
   const b = [];
@@ -15,39 +7,23 @@ export default function solution(br, w, tw) {
     console.log("start", tw, b, cnt, b.length);
 
     // 추가
+    if (b.length === br) b.shift();
+
     if (tw.length !== 0) {
       const sum = b.reduce((acc, v) => acc + v, 0) + tw[0];
-      if (b.length !== br) {
-        if (sum <= w) {
-          const t = tw.shift();
-          b.push(t);
-          console.log("add");
-        } else {
-          // 전진
-          b.push(null);
-          console.log("move forward");
-        }
+      if (sum <= w) {
+        const t = tw.shift();
+        b.push(t);
+        console.log("add");
       } else {
-        // 탈출
-        b.shift();
-        const sum = b.reduce((acc, v) => acc + v, 0) + tw[0];
-        if (sum <= w) {
-          const t = tw.shift();
-          b.push(t);
-        } else {
-          b.push(null);
-        }
-        console.log("escape");
+        // 전진
+        b.push(null);
+        console.log("move forward");
       }
     } else {
-      if (b.length !== br) {
-        b.push(null);
-      } else {
-        // 탈출
-        b.shift();
-        b.push(null)
-        console.log("escape end");
-      }
+      // 탈출
+      b.push(null);
+      console.log("escape end");
     }
 
     cnt++;
@@ -61,82 +37,3 @@ export default function solution(br, w, tw) {
 
   return answer;
 }
-
-/*
-
-
-  while (true) {
-    console.log(tw, b, cnt, b.length);
-
-    if (b.length >= br) {
-      b.pop();
-      b.unshift(null);
-      cnt++;
-      console.log("b ft", b);
-    }
-
-    if (tw.length === 0 && b.length !== br) b.unshift(null);
-    console.log(b);
-
-    if (b.every((_) => _ === null) && b.length === br && tw.length === 0) {
-      console.log("eve");
-      answer = cnt;
-      break;
-    }
-
-    if (tw.length !== 0) {
-      const r = b.reduce((acc, v) => acc + v, 0) + tw[0];
-      console.log("r", r);
-
-      if (r > w) {
-        b.unshift(null);
-      } else {
-        const t = tw.shift();
-        b.unshift(t);
-      }
-    }
-
-    // console.log("t", t);
-    cnt++;
-
-    console.log("un", tw, b, cnt, b.length);
-
-    // console.log(tw.length !== 0, b.length !== 0);
-
-    if (cnt === 20) break;
-    // if (!(tw.length !== 0 || b.length !== 0)) break;
-  }
-
-  answer = cnt;
-  return answer;
-
-*/
-
-/*
-  switch (act) {
-    case "추가":
-      // 추가
-      if (tw.length !== 0) {
-        const sum = b.reduce((acc, v) => acc + v, 0) + tw[0];
-        if (b.length !== br && sum <= w) {
-          const t = tw.shift();
-          b.push(t);
-        }
-      }
-      break;
-    case "전진":
-      // 전진
-      if (b.length !== br) {
-        b.push(null);
-      }
-
-      break;
-    case "탈출":
-      // 탈출
-      if (b.length === br) b.shift();
-
-      break;
-  }
-  cnt++;
-
-*/
