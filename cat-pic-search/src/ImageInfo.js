0957f05c17b6314a705496a0b8f3bf5eaa2cf7df
tolluset <dlqud19@gmail.com>
2022-01-24 05:47:07 +0900

    feat: add close when click oustide of modal, press esc and click close button

--- cat-pic-search/src/ImageInfo.js
+++ cat-pic-search/src/ImageInfo.js
@@ -1,31 +1,45 @@
 class ImageInfo {
-    $imageInfo = null;
-    data = null;
-  
-    constructor({ $target, data }) {
-      this.$imageInfo = document.createElement("div");
-      this.$imageInfo.className = "ImageInfo";
-      $target.appendChild(this.$imageInfo);
-  
-      this.data = data;
-  
-      this.render();
-    }
-  
-    setState(nextData) {
-      this.data = nextData;
-      this.render();
-    }
-  
-    render() {
-      if (this.data.visible) {
-        const { name, url, temperament, origin } = this.data.image;
-  
-        this.$imageInfo.innerHTML = `
-          <div class="content-wrapper" role="dialog" aria-modal="true">
+  $imageInfo = null;
+  data = null;
+
+  constructor({ $target, data }) {
+    this.$imageInfo = document.createElement("div");
+    this.$imageInfo.className = "ImageInfo";
+    $target.appendChild(this.$imageInfo);
+
+    window.addEventListener("keyup", (e) => {
+      if (e.code === "Escape") {
+        this.close();
+      }
+    });
+
+    window.addEventListener("click", (e) => {
+      e.target.className === "ImageInfo" ? this.close() : false;
+    });
+
+    this.data = data;
+
+    this.render();
+  }
+
+  setState(nextData) {
+    this.data = nextData;
+    this.render();
+  }
+
+  close() {
+    this.$imageInfo.style.display = "none";
+  }
+
+  render() {
+    if (this.data.visible) {
+      const { name, url, temperament, origin } = this.data.image;
+
+      this.$imageInfo.innerHTML = `
+          <div class="content-wrapper" role="dialog" aria-modal="true" tabindex=0>
             <div class="title">
               <span>${name}</span>
-              <div class="close">x</div>
+              <div class="close" onClick="close">x</div>
             </div>
             <img src="${url}" alt="${name}"/>        
             <div class="description">
@@ -33,10 +47,15 @@ class ImageInfo {
               <span>태생: ${origin}</span>
             </div>
           </div>`;
-        this.$imageInfo.style.display = "block";
-      } else {
-        this.$imageInfo.style.display = "none";
-      }
+      this.$imageInfo.style.display = "block";
+
+      const $close = document.getElementsByClassName("close")[0];
+
+      $close.addEventListener("click", () => {
+        this.close();
+      });
+    } else {
+      this.$imageInfo.style.display = "none";
     }
   }
-  
+}
