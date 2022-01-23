da9568ef2b81eda65a75d7ffd282713f24fcabd9
tolluset <dlqud19@gmail.com>
2022-01-23 19:34:25 +0900

    feat: add dark mode toggle button

--- cat-pic-search/src/App.js
+++ cat-pic-search/src/App.js
@@ -7,6 +7,10 @@ class App {
   constructor($target) {
     this.$target = $target;
 
+    this.darkModeToggleButtojn = new DarkModeToggleButton({
+      $target
+    });
+
     this.searchInput = new SearchInput({
       $target,
       onSearch: keyword => {
