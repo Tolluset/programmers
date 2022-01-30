1163ff883b1365d110597f94e1d046957059b98b
tolluset <dlqud19@gmail.com>
2022-01-30 19:57:44 +0900

    feat: reset input field when focus input field

--- cat-pic-search/src/SearchInput.js
+++ cat-pic-search/src/SearchInput.js
@@ -8,13 +8,17 @@ class SearchInput {
     this.$searchInput.className = "SearchInput";
     $target.appendChild(this.$searchInput);
 
+    this.$searchInput.focus();
+
     this.$searchInput.addEventListener("keyup", (e) => {
       if (e.code === "Enter") {
         onSearch(e.target.value);
       }
     });
 
-    this.$searchInput.focus();
+    this.$searchInput.addEventListener("focus", () => {
+      this.$searchInput.value = "";
+    })   
 
     console.log("SearchInput created.", this);
   }
