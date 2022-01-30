09d4b73dee47ca3b519a44c522fb95900fe48475
tolluset <dlqud19@gmail.com>
2022-01-30 19:40:12 +0900

    refactor: using transition for modal fade in/out

--- cat-pic-search/src/ImageInfo.js
+++ cat-pic-search/src/ImageInfo.js
@@ -29,14 +29,25 @@ class ImageInfo {
   }
 
   open() {
-    this.$imageInfo.classList.remove("fadeout");
     this.$target.appendChild(this.$imageInfo);
+    console.log(1);
+
+    setTimeout(() => {
+      this.$imageInfo.style.opacity = 1;
+      this.$imageInfo.style.transition = "all 1s";
+    });
+
+    // this.$imageInfo.classList.add("fadein");
+    // this.$imageInfo.classList.remove("fadeout");
   }
 
   close() {
-    this.$imageInfo.classList.add("fadeout");
+    this.$imageInfo.style.opacity = 0;
+    this.$imageInfo.style.transition = "all 1s";
+
+    // this.$imageInfo.classList.add("fadeout");
     this.$imageInfo.addEventListener(
-      "animationend",
+      "transitionend",
       () => {
         this.$target.removeChild(this.$imageInfo);
       },
